02afd6b4fbb32b5a00fa9b8adbb8d8fe3543e364
sq2ips <pawel.sq2ips@gmail.com>
2026-02-19 20:16:47 +0100

    parameters check

--- include/sstv.h
+++ include/sstv.h
@@ -9,12 +9,12 @@
 
 #define SSTV_PIN 15
 
-#define SSTV_PWM_CLKDIV 2 // PWM clock divider value f_pwm = f_clock/(CLKDIV*(WRAP+1))
+#define SSTV_PWM_CLKDIV 4 // PWM clock divider value f_pwm = f_clock/(CLKDIV*(WRAP+1))
 #define SSTV_PWM_WRAP 255 // PWM counter wrap value, must be max value of sine table
 
 #define SSTV_ALARM_NUM 0
 #define SSTV_ALARM_IRQ TIMER_IRQ_0
-#define SSTV_SAMPLE_RATE 17856
+#define SSTV_SAMPLE_RATE ((1200 * 16) * 0.93)
 #define SSTV_ALARM_TIME_MS (1000000/SSTV_SAMPLE_RATE)
 #define SSTV_PHASE_INC_1HZ (4294967295 / SSTV_SAMPLE_RATE) // phase_inc wrap value divided by phase increase interrupt rate gives a phase increase value for 1Hz
 
