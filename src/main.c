12944e70bfec16b16b40a09d654f8af591118413
sq2ips <pawel.sq2ips@gmail.com>
2026-04-03 15:50:49 +0200

    Added watchdog

--- src/main.c
+++ src/main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
-#include "hardware/irq.h"
-
-#include "pico/multicore.h"
+#include "hardware/watchdog.h"
 
 #include "sstv.h"
 #include "radio.h"
@@ -15,6 +12,14 @@ extern const uint8_t _binary_image_bin_end[];
 #define BINARY_IMAGE_BIN_LEN (_binary_image_bin_end-_binary_image_bin_start)
 #define SSTV_COUNT (BINARY_IMAGE_BIN_LEN/SSTV_BUFF_LEN)
 
+void sleep_wd(int32_t time){
+    while(time > 0){
+        watchdog_update();
+        sleep_ms(25);
+        time-=25;
+    }
+}
+
 int main() {
     stdio_init_all();
     //while(!stdio_usb_connected()){sleep_ms(10);}
@@ -22,17 +27,20 @@ int main() {
 
     radio_init();
 
+    watchdog_enable(100, 1);
+    printf("Watchdog enabled.");
+
     hard_assert(SSTV_BUFF_LEN*SSTV_COUNT == BINARY_IMAGE_BIN_LEN);
 
     while (true) {
         for(uint8_t cnt = 0; cnt<SSTV_COUNT; cnt++){
             radio_write(REG_OP_MODE, MODE_TX);
-            sleep_ms(SSTV_WAIT);
+            sleep_wd(SSTV_WAIT);
             start_sstv(_binary_image_bin_start+cnt*SSTV_BUFF_LEN);
-            while(sstv_running) sleep_ms(100);
-            sleep_ms(SSTV_WAIT);
+            while(sstv_running) sleep_wd(100);
+            sleep_wd(SSTV_WAIT);
             radio_write(REG_OP_MODE, MODE_SLEEP);
-            sleep_ms(SSTV_DELAY);
+            sleep_wd(SSTV_DELAY);
         }
     }
 }
