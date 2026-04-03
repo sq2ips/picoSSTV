e3e10b514ffc7aeae2e4a3a6235cb15bb331a7cf
sq2ips <pawel.sq2ips@gmail.com>
2026-04-03 16:22:43 +0200

    Added LED

--- src/main.c
+++ src/main.c
@@ -21,6 +21,10 @@ void sleep_wd(int32_t time){
 }
 
 int main() {
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+    
     stdio_init_all();
     //while(!stdio_usb_connected()){sleep_ms(10);}
     printf("picoSSTV starting...\n");
@@ -34,12 +38,16 @@ int main() {
 
     while (true) {
         for(uint8_t cnt = 0; cnt<SSTV_COUNT; cnt++){
+            gpio_put(PICO_DEFAULT_LED_PIN, 1);
+            printf("Starting SSTV transmission of image %d...\n", cnt);
             radio_write(REG_OP_MODE, MODE_TX);
             sleep_wd(SSTV_WAIT);
             start_sstv(_binary_image_bin_start+cnt*SSTV_BUFF_LEN);
+            gpio_put(PICO_DEFAULT_LED_PIN, 0);
             while(sstv_running) sleep_wd(100);
             sleep_wd(SSTV_WAIT);
             radio_write(REG_OP_MODE, MODE_SLEEP);
+            printf("Transmission ended.");
             sleep_wd(SSTV_DELAY);
         }
     }
